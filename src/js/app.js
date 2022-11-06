61834043383eca52efe14190229e9da528a07922
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-11-06 23:17:02 +0500

    Render player moves on game board

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,29 @@
 // tic tac toe gameboard - revealing modular pattern
 
 const GameBoard = (function() {
-  // private tic tac toe gameboard array
+  // init 
   let _gameBoard = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    {playerName: 'player1', marker: 'x'}, {playerName: 'player2', marker: 'o'}, {playerName: 'player1', marker: 'x'},
+    {playerName: 'player2', marker: 'o'}, {playerName: 'player1', marker: 'x'}, {playerName: 'player1', marker: 'x'},
+    {playerName: 'player2', marker: 'o'}, {playerName: 'player2', marker: 'o'}, {playerName: 'player1', marker: 'x'}
   ];
-  // functions needed
-  // update gameboard 
+
+  // cache DOM
+  const game = document.querySelector('#game-id');
+  const ticTacToeBoard = game.querySelector('.tic-tac-toe-game-board');
+  const gameCell = ticTacToeBoard.querySelectorAll('.game-cell');
+
+  render();
+
+  // render player move on board
+  function render() {
+    for(let cell = 0; cell < gameCell.length; cell++) {
+      gameCell[cell].innerHTML = `
+        <img src="./assets/marker-${_gameBoard[cell].marker}.png" alt="${_gameBoard[cell].marker}">
+      `;
+    }
+  }
+
 })();
 
 // player - factory function
@@ -22,7 +37,7 @@ const player2 = playerFactory('John', 'O');
 console.log(player2)
 
 
-document.querySelector('.btn-rules').addEventListener('click', showRules, false);
+/*document.querySelector('.btn-rules').addEventListener('click', showRules, false);*/
 
 function showRules() {
   document.querySelector('.rules-section').classList.add('show');
